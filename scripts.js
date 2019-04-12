f5852dafd82f1eab874c3a0088df4f99f1d3bdaf
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-12 11:23:59 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,26 +9,26 @@ app.appendChild(container1);
 var request = new XMLHttpRequest();
 
 //GET REQUEST WITH USE OF HEROKU AS A PROXY TO SOLVE CORS ERROR
-request.open('GET','https://cors-anywhere.herokuapp.com/', 
+request.open('GET','https://cors-anywhere.herokuapp.com/https://api.punkapi.com/v2/beers?ibu_lt=36', 
 true);
 
 request.onload = function () {  
 // CONVERT JSON DATA TO JAVASCRIPT OBJECTS USING JSON.PARSE
     var apiData = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-        apiData.forEach(job => {
+        apiData.forEach(beer => {
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
 
-      // render relevant data: job title, job type, location and description      
+      // render relevant data: beer name, description and image      
         const h1 = document.createElement('h1');
-        h1.textContent = job.title;
+        h1.textContent = beer.name;
             
         const h3 = document.createElement('h3');
-        h3.textContent = job.type;
+        h3.textContent = beer.description;
             
         const h4 = document.createElement('h4');
-        h4.textContent = job.location;   
+        h4.textContent = beer.image_url;   
             
         const p = document.createElement('p');
         job.description = job.description.substring(0, 300);
