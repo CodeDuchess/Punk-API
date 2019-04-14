139d6dd4e4899321c6ca22e2f867ad42ce557ee4
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-14 19:04:19 -0400

    Create scripts2.js

---
+++ scripts2.js
@@ -0,0 +1,68 @@
+'use strict';
+var apiData = "";
+var filteredBeers= "";
+
+Vue.component('beer-card', {
+    props: ['beer'],
+    template: '<div class="card"><h2>{{ beer.abv }}</h2><h3>{{ this.beerIbu }}</h3></div>',
+    computed: {
+        beerIbu: function() {
+            return this.beer.ibu;
+        }
+    }
+  });
+
+  var vm = new Vue({
+	el:  "#beers",
+	data() { 
+	  return {
+             apiData:[],
+             search: '',
+             filterType: [],
+          
+             selectedType:"beer abv"
+        }
+    },
+      
+    computed: {
+		filteredBeers: function() {
+			vm = this;
+			var type = vm.selectedType;
+			
+			if(type == "beer abv") {
+				return vm.apiData;
+			} else {
+				return vm.apiData.filter(function(beer) {
+					return beer.abv == abv;
+				});
+            }
+		}
+    },
+
+    methods: { 
+        getBeerData: function() {
+            let url = 'https://cors-anywhere.herokuapp.com/https://api.punkapi.com/v2/beers';
+            fetch( url,{
+                'headers': {
+                  'Content-Type': 'application/json'
+                },
+              })
+              .then((response) => {
+                return response.json();
+              
+              })
+              .then((rJson) => {
+                  this.apiData = rJson;
+              })
+        }
+    },
+
+    mounted: function() {
+        this.$nextTick(function() {
+            this.getBeerData();
+        });
+    }
+
+
+});
+  
