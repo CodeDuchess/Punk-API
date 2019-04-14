815a5323c70f69634d7650c6a4d2f26a037002f8
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-14 09:17:09 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -73,17 +73,6 @@ function showResults1(){
   }
 }
 
-// VUE.JS INSTANCE HERE
- new Vue ({
-     el: '#app',
-     data() {
-         return {
-             title: 'Click Here for More!',
-             show: false,
-             description: 'Wheat is Neat Event will take place on May 2, 2019 at BrewDog Brewery!'
-                }
-             }
-        )}; 
           
 request.send();
 
