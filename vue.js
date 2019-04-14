04375a0609317063e76aac3863e32e064b3c91b3
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-14 09:17:48 -0400

    Update vue.js

--- vue.js
+++ vue.js
@@ -1,1 +1,12 @@
 
+// VUE.JS INSTANCE HERE
+ new Vue ({
+     el: '#app',
+     data() {
+         return {
+             title: 'Click Here for More!',
+             show: false,
+             description: 'Wheat is Neat Event will take place on May 2, 2019 at BrewDog Brewery!'
+                }
+             }
+        )}; 
