d0e448db32165a33b713b5eb3a0757c7f4a0a52b
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-15 09:51:06 -0400

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -7,7 +7,7 @@ Vue.component('beer-card', {
     computed: {
         beerFood_pairing: function() {
             
-            return this.beer.food_pairing.substring(0, 100) + '...';
+            return this.beer.foodPairing.substring(0, 100) + '...';
         }
     }
   });
